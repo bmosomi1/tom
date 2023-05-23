d150055a427d86d5ff6ce68630cba90b6f4d9f43
Benard Mosomi <benardmosomi@gmail.com>
2023-05-23 09:48:27 +0300

    chages

--- sms/views.py
+++ sms/views.py
@@ -741,7 +741,7 @@ def multi_search(request):
                 payments = WaterPaymentReceived.objects.raw('SELECT * FROM sms_waterpaymentreceived where account_number= %s or dest_msisdn=%s orderby id desc', [search_key, search_key])
                 #payments = WaterPaymentReceived.objects.filter(confirmation_code__exact=search_key)
                 readings = WaterMeterReadings.objects.raw('SELECT * FROM sms_watermeterreadings where account_number_id= %s or msisdn=%s order by id desc', [search_key, search_key])
-                bills_sent = WaterBillSent.objects.raw('SELECT * FROM sms_waterbillsent where account_number_id= %s or dest_msisdn=%s order by id desc', [search_key, search_key])
+                bill_sent = WaterBillSent.objects.raw('SELECT * FROM sms_waterbillsent where account_number_id= %s or dest_msisdn=%s order by id desc', [search_key, search_key])
                 messages_sent = WaterOutbox.objects.raw('SELECT * FROM water_outbox where dest_msisdn=%s order by id desc', [search_key])
             except ValueError:
                 if len(search_key)>2:
@@ -754,7 +754,7 @@ def multi_search(request):
                 payments = WaterPaymentReceived.objects.raw('SELECT * FROM green_note.sms_waterpaymentreceived where account_number= %s or dest_msisdn=%s order by id desc', [search_key, search_key])
                 #payments = WaterPaymentReceived.objects.filter(account_number=search_key)
                 readings = WaterMeterReadings.objects.raw('SELECT * FROM sms_watermeterreadings where account_number_id= %s or msisdn=%s order by id desc', [search_key, search_key])
-                bills_sent = WaterBillSent.objects.raw('SELECT * FROM sms_waterbillsent where account_number_id= %s or dest_msisdn=%s order by id desc', [search_key, search_key])
+                bill_sent = WaterBillSent.objects.raw('SELECT * FROM sms_waterbillsent where account_number_id= %s or dest_msisdn=%s order by id desc', [search_key, search_key])
                 messages_sent = WaterOutbox.objects.raw('SELECT * FROM water_outbox where dest_msisdn=%s order by id desc', [search_key])
 
 
